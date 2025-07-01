c670bd314d71a30f19a74cf8755a3d73dd47df07
Luke Chamberland <luke.chamberland@shaw.ca>
2025-06-30 21:56:08 -0600

    add list functionality

--- app/list-components/List.tsx
+++ app/list-components/List.tsx
@@ -3,6 +3,8 @@ import { useState, useEffect } from "react";
 import { Fruit } from "../interfaces.ts";
 
 export default function List() {
+  const [list, setList] = useState<Fruit[]>([]);
+
   useEffect(() => {
     async function fetchFruits(): Promise<Fruit[]> {
       const data = await fetch("https://fruity-proxy.vercel.app/api/fruits", {
@@ -13,6 +15,7 @@ export default function List() {
 
       const parsedData = await data.json();
       console.log(parsedData);
+      setList(parsedData);
     }
 
     fetchFruits();
@@ -20,8 +23,14 @@ export default function List() {
 
   function FruitList() {
     return (
-      <div className="flex justify-center border border-black-500 w-full">
+      <div className="justify-center border border-black-500 w-full">
         <div>
+          <div>
+            <select name="view" id="view">
+              <option value="List">List View</option>
+              <option value="List">Table View</option>
+            </select>
+          </div>
           <label htmlFor="fruit property">Group by</label>
           <select name="fruit property" id="fruit property">
             <option value="None">None</option>
@@ -29,10 +38,15 @@ export default function List() {
             <option value="Order">Order</option>
             <option value="Genus">Genus</option>
           </select>
-          <select name="view" id="view">
-            <option value="List">List View</option>
-            <option value="List">Table View</option>
-          </select>
+        </div>
+        <div>
+          {list
+            ? list.map((fruit) => (
+                <h3 key={fruit.id}>
+                  {fruit.name} ({fruit.nutritions.calories})
+                </h3>
+              ))
+            : null}
         </div>
       </div>
     );
