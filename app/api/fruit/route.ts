ed5204846ac890be98a225a23147e603e64db67a
Luke Chamberland <luke.chamberland@shaw.ca>
2025-07-02 17:44:02 -0600

    fix proxy

--- app/api/fruit/route.ts
+++ app/api/fruit/route.ts
@@ -11,14 +11,29 @@ export async function GET(req: Request) {
       },
     });
 
+    // const response = await fetch(
+    //   "https://corsproxy.io/?https://fruity-proxy.vercel.app/api/fruits",
+    //   {
+    //     headers: {
+    //       "x-api-key": "fruit-api-challenge-2025",
+    //     },
+    //   }
+    // );
+
+    console.log(response);
+
     const data = await response.json();
 
     const res = NextResponse.json(data, { status: 200 });
     res.headers.set("Access-Control-Allow-Origin", ALLOWED_ORIGIN);
     res.headers.set("Access-Control-Allow-Methods", "GET, OPTIONS");
-    res.headers.set("Access-Control-Allow-Headers", "Content-Type, x-api-key");
+    res.headers.set(
+      "Access-Control-Allow-Headers",
+      "Content-Type, x-api-key, Origin"
+    );
     return res;
   } catch (error) {
+    console.log(error);
     return NextResponse.json(
       { error: "Internal Server Error" },
       { status: 500 }
@@ -26,13 +41,22 @@ export async function GET(req: Request) {
   }
 }
 
-export function OPTIONS() {
+export function OPTIONS(req) {
+  req.headers.set("x-api-key", "fruit-api-challenge-2025");
   const res = new Response(null, {
     status: 204,
+    headers: {
+      "Access-Control-Allow-Headers": "Content-Type, x-api-key, Origin",
+      "Access-Control-Allow-Methods": "GET, OPTIONS",
+      "Access-Control-Allow-Origin": ALLOWED_ORIGIN
+    },
   });
 
   res.headers.set("Access-Control-Allow-Origin", ALLOWED_ORIGIN);
   res.headers.set("Access-Control-Allow-Methods", "GET, OPTIONS");
-  res.headers.set("Access-Control-Allow-Headers", "Content-Type, x-api-key");
+  res.headers.set(
+    "Access-Control-Allow-Headers",
+    "Content-Type, x-api-key, Origin"
+  );
   return res;
 }
